2c21a2f16b51e91545fb777db9520b36a3d081dc
alex <alex@canete.org>
2022-01-02 11:36:52 +0100

    recAccesoadato proyecto creado

---
+++ recAcceso/src/controller/Controlador.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class Controlador {
+
+    public static void leerArchivos() {
+        
+    }
+
+
+    public static void exportarEnFormatoXML() {
+    }
+
+    public static void exportarEnObjetoList() {
+    }
+
+    public static void exportarEnOracle() {
+    }
+
+
+
+
+}
---
+++ recAcceso/src/view/Principal.java
@@ -0,0 +1,15 @@
+package view;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		controller.Controlador.leerArchivos();
+		controller.Controlador.exportarEnFormatoXML();
+		controller.Controlador.exportarEnObjetoList();
+		controller.Controlador.exportarEnOracle();
+
+
+
+	}
+
+}
